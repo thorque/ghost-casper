ae1eeddbe138d5b3188af596f963477ada584505
Thorsten Kamann <thorsten@pictor.photography>
2025-04-19 16:36:32 +0200

    Adding cookie consent banner and adding link sytyling

--- gulpfile.js
+++ gulpfile.js
@@ -80,6 +80,15 @@ function jquery(done) {
     ], handleError(done));
 }
 
+function cookieconsent(done) {
+    pump([
+        src(['node_modules/vanilla-cookieconsent/dist/*.js',
+        'node_modules/vanilla-cookieconsent/dist/*.css']),
+        dest('assets/built/', {sourcemaps: '.'}),
+        livereload()
+    ], handleError(done));
+}
+
 function zipper(done) {
     const filename = require('./package.json').name + '.zip';
 
@@ -101,7 +110,7 @@ const cssWatcher = () => watch('assets/css/**', css);
 const jsWatcher = () => watch('assets/js/**', js);
 const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
 const watcher = parallel(cssWatcher, jsWatcher, hbsWatcher);
-const build = series(css, jquery, js);
+const build = series(css, jquery, cookieconsent, js);
 
 exports.build = build;
 exports.zip = series(build, zipper);
